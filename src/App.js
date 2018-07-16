91e4f06b76ca6f8d346df753780a73226c219ccf
Liu Jiahuan <liujiahuan@xuetangx.com>
2018-07-16 13:43:19 +0800

    变量赋值

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+const list = [{
+  title: 'React',
+  url: 'https://facebook.github.io/react/',
+  author: 'React',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+}, {
+  title: 'Redux',
+  url: 'https://github.com/reactjs/redux',
+  author: 'React',
+  num_comments: 5,
+  points: 2,
+  objectID: 1,
+}]
 class App extends Component {
   render() {
     const helloWorld = 'Welcome to React 肯定的';
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <h2>{helloWorld}</h2>
+        {
+          list.map(function(item) {
+            return <div>{item.title}</div>
+          })
+        }
       </div>
     );
   }
