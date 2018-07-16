dadcf7eb87fbf1244a52af4267b12a97268dc5f2
Liu Jiahuan <liujiahuan@xuetangx.com>
2018-07-16 16:36:31 +0800

    函数式无状态组件

--- src/App.js
+++ src/App.js
@@ -58,7 +58,9 @@ class App extends Component {
         <Search
           value={searchTerm}
           onChange={this.onSearchChange}
-        />
+        >
+          Search
+        </Search>
         <Table
           list={list}
           pattern={searchTerm}
@@ -69,49 +71,48 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange } = this.props;
-    return (
-      <form>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    )
-  }
-}
+// 函数式无状态组件 - 查找
+const Search = ({value, onChange, children}) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => {
-          return (
-            <div key={item.objectID}>
-              <span>
-                  <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={() => onDismiss(item.objectID)}
-                  type="button"
-                >
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          );
-        })}
-      </div>
-    )
-  }
-}
+// 函数式无状态组件 - 列表
+const Table = ({list, pattern, onDismiss}) =>
+  <div>
+    {list.filter(isSearched(pattern)).map(item => {
+      return (
+        <div key={item.objectID}>
+          <span>
+              <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            <Button
+              onClick={() => onDismiss(item.objectID)}
+            >
+              Dismiss
+            </Button>
+          </span>
+        </div>
+      );
+    })}
+  </div>
+
+// 函数式无状态组件 - 通用button组件
+const Button = ({ onClick, className = '', children }) => 
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
 
 export default App;
