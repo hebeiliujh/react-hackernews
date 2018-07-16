f49df279d1b4348c66c06026109dcc9b920e074f
Liu Jiahuan <liujiahuan@xuetangx.com>
2018-07-16 13:49:37 +0800

    箭头函数的使用

--- src/App.js
+++ src/App.js
@@ -19,14 +19,20 @@ const list = [{
 }]
 class App extends Component {
   render() {
-    const helloWorld = 'Welcome to React 肯定的';
     return (
       <div className="App">
-        {
-          list.map(function(item) {
-            return <div>{item.title}</div>
-          })
-        }
+        {list.map(item => {
+          return (
+            <div key={item.objectID}>
+              <span>
+                  <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })}
       </div>
     );
   }
